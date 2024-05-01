3ca37bd9183d22b0c6355b8fecb1fe7fe08bb239
Mhdnaveed18 <mhdnaveed18@gmail.com>
2024-05-01 15:36:25 +0530

    changed the text size for mobile screen

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
   return (
     <div className='flex flex-col lg:flex-row justify-around items-center w-full h-[100vh-3.5rem] py-2 text-xl border-b-2 '>
     <Link to={'/'}><img src={logo} width={150} alt='image10' className='text-xl '></img></Link>
-      <div className='flex gap-2 md:gap-10 '>
+      <div className='flex gap-2 md:gap-10 text-base md:text-xl'>
         <Link to={'/projects'} className={`${checklocation('/projects') ? 'orange-gradient' :''}`} >Projects</Link>
         <Link to={'/skills'} className={`${checklocation('/skills') ? 'orange-gradient' :''}`} >Skills</Link>
         <Link to={'/Education'} className={`${checklocation('/Education') ? 'orange-gradient' :''}`} >Education</Link>
